afe97049682fba8839ea5719fbe6454a6f34b079
JorgeMano <jorge.l.manosalva@gmail.com>
2022-05-04 23:57:09 -0500

    First Commit

---
+++ portafolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import introduction from './components/introduction/introduction';
+import './App.css';
+
+const App = () => {
+    return (
+      <div className="App">
+        <introduction />       
+      </div>
+    );
+  }
+
+export default App;
---
+++ portafolio/src/components/introduction/introduction.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const introduction = () => {
+  return (
+    <div>
+        this is introduction
+    </div>
+  )
+}
+
+export default introduction
